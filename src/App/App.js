924039589c8ab45d278f0ce4d452301c2b52f9b9
Billy Campbell <wcampbell91@gmail.com>
2020-08-08 11:18:29 -0500

    setup files for react app

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>INSIDE THE APPPPPP!</h2>
+        <button className="btn btn-info"><i class="fas fa-id-card"></i>PINTEREST BUTTON WOOT!</button>
+      </div>
+    );
+  }
+}
+
+export default App;
