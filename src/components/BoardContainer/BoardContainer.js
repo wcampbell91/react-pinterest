213f64f4bd9878f4cbd758c773109102ccd6c974
Billy Campbell <wcampbell91@gmail.com>
2020-08-17 20:29:21 -0500

    added authentication

---
+++ src/components/BoardContainer/BoardContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class BoardContainer extends React.Component {
+  render() {
+    return (
+      <h2>In BoardContainer!</h2>
+    );
+  }
+}
+
+export default BoardContainer;
